77f06ded8bc6b9130b6349dfe863aa6181d28903
KymCat <kohina1452@gmail.com>
2025-08-29 15:26:11 +0900

    Broker 코드 작성 및 핸들러 패키지 생성

--- src/main/java/config/WebSocketConfig.java
+++ src/main/java/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package config;
 
+import config.handler.ChatWebSocketHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
